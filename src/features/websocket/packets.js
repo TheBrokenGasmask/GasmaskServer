a6a814a3977ba3997678c11599cf2135cbae1ea8
iron <aaa@aaa.com>
2025-08-22 04:02:39 -0400

    Update packets.js

--- src/features/websocket/packets.js
+++ src/features/websocket/packets.js
@@ -12,6 +12,8 @@ const PACKET_TYPES = {
     RANK_PROMOTION_REQUEST: 'rank_promotion_request',
     RANK_PROMOTION_RESPONSE: 'rank_promotion_response',
     CHAT_ANNOUNCEMENT: 'chat_announcement',
+    RAID_REPORT: 'raid_report',
+    RAID_REPORT_ACK: 'raid_report_ack',
     CONNECT: 'connect',
     DISCONNECT: 'disconnect',
     PONG: 'pong',
@@ -27,6 +29,11 @@ const pingHandler = async (client, packet) => {
             clientId: client.id
         }
     };
+};
+
+const raidReportHandler = async (client, packet) => {
+    const { raidReport } = require('../raids/report-raid-endpoint');
+    return await raidReport.handleRaidReport(client, packet);
 };
 
 const heartbeatHandler = async (client, packet) => {
@@ -95,6 +102,7 @@ const PACKET_HANDLERS = {
     [PACKET_TYPES.PING]: pingHandler,
     [PACKET_TYPES.HEARTBEAT]: heartbeatHandler,
     [PACKET_TYPES.CHAT_MESSAGE]: chatMessageHandler,
+    [PACKET_TYPES.RAID_REPORT]: raidReportHandler,
     [PACKET_TYPES.RANK_PROMOTION_RESPONSE]: rankPromotionResponseHandler,
     [PACKET_TYPES.CHAT_ANNOUNCEMENT]: chatAnnouncementHandler,
     [PACKET_TYPES.CONNECT]: connectHandler,
@@ -107,6 +115,7 @@ module.exports = {
     pingHandler,
     heartbeatHandler,
     chatMessageHandler,
+    raidReportHandler,
     rankPromotionResponseHandler,
     chatAnnouncementHandler,
     connectHandler,
