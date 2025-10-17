0afdeb3ce95e3aba3eebd37dc9118ab71d4027c2
wiji <wijigaming.yt@gmail.com>
2025-10-17 10:51:05 -0700

    testing

--- src/features/player/players-endpoint.js
+++ src/features/player/players-endpoint.js
@@ -41,6 +41,8 @@ class PlayersEndpoint {
                         needs_aspects: player.needs_aspects
                     });
 
+                    console.log(player.username, player.guild_rank);
+
                     const playerData = {
                         uuid: player.uuid,
                         username: player.username,
