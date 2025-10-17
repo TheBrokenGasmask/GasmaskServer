435469401b5f5a2e5488bb5a74c9e5a499c8bdc1
wiji <wijigaming.yt@gmail.com>
2025-10-17 12:15:50 -0700

    testing

--- src/features/player/players-endpoint.js
+++ src/features/player/players-endpoint.js
@@ -46,7 +46,7 @@ class PlayersEndpoint {
                         username: player.username,
                         guild: player.guild,
                         rank: rankInfo,
-                        multiple_ranks: (discordId ? rankMap.get(discordId).star : false),
+                        multiple_ranks: (rankMap.has(discordId) ? rankMap.get(discordId).star : false),
                         needs_aspects: player.needs_aspects,
                         badges: playerBadges,
                         has_discord_link: !!discordId
