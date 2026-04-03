9c23ad7ee9348eb17d9a196e4a60969699dd962a
wiji <wijigaming.yt@gmail.com>
2026-04-03 16:18:12 -0700

    change aspects command to owed card

--- src/discord/commands/aspects-command.js
+++ src/discord/commands/aspects-command.js
@@ -1,7 +1,7 @@
-const { SlashCommandBuilder, AttachmentBuilder, EmbedBuilder } = require("discord.js");
-const axios = require('axios');
+const { SlashCommandBuilder, AttachmentBuilder } = require("discord.js");
 const {getPlayerUUID, getRaids, getPlayerUsername, getAspects, getGuild} = require("../../core/database");
 const { config } = require("../../core/config");
+const {createAspectsCard} = require("../../discord/image-generation/aspects-card");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -35,22 +35,12 @@ module.exports = {
         let totalAspects = aspectData.length;
         let owedAspects = Math.max(Math.floor(raidData.length / 2) - totalAspects, 0);
 
-        const response = await axios.get(`https://crafatar.com/renders/head/${uuid}?overlay=true`, { responseType: 'arraybuffer' });
-        const buffer = Buffer.from(response.data, 'binary');
-        const attachment = new AttachmentBuilder(buffer, { name: 'thumbnail.png' });
+        const cardBuffer = await createAspectsCard(uuid, playerName, raidData.length, totalAspects, owedAspects);
 
-        const exampleEmbed = new EmbedBuilder()
-            .setColor(0x0099FF)
-            .setAuthor({ name: 'Player Guild Aspects' })
-            .setTitle(`**${playerName}**`)
-            .setThumbnail('attachment://thumbnail.png')
-            .addFields(
-                { name: '\u200B', value: '\u200B' },
-                { name: 'Total Raids Completed', value: `\`\`\`${raidData.length}\`\`\`` },
-                { name: "Aspects Given", value: `\`\`\`${totalAspects}\`\`\``},
-                { name: 'Owed Aspects', value: `\`\`\`${owedAspects}\`\`\``},
-            )
+        const attachment = new AttachmentBuilder(cardBuffer, {
+            name: 'aspects-card.png'
+        });
 
-        await interaction.reply({ embeds: [exampleEmbed], files: [attachment] });
+        await interaction.reply({ files: [attachment] });
     },
 };
